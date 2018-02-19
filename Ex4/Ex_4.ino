9430ac4e92f9555293a7f6ec9f17b43fa0682abf
MariusBaltes <marius.baltes.79@lacetania.com>
2018-02-19 16:33:45 +0100

    1era pujada

---
+++ Ex4/Ex_4.ino
@@ -0,0 +1,31 @@
+/******************************************************************
+**                                                               **
+**                             EX_4                              **      
+**                                                               **
+**                                                               ** 
+**  BALTES MARIUS                           DATA: 17/02/2018     **            
+*******************************************************************/
+//**************************** INCLUDE ****************************
+
+
+//*************************** VARIABLES ***************************
+
+//***************************** SETUP *****************************
+
+
+void setup(){ Serial.begin(9600);
+
+
+Serial.println("Escull el numero de l'operació que vols realitzar");
+Serial.println("1.comprova número de la targeta");
+Serial.println("2.comprova el número de compte bancari");
+Serial.println("3.buscar un dígit perdut de la targeta de crèdit");
+
+  }
+
+//******************************* LOOP ****************************
+
+void loop() {
+}
+
+//******************************FUNCIONS***************************
